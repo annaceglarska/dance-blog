4b9071618c23aade60803caf6c8621bf5cb1a691
Anna Kikut <aniaceglarska1@gmail.com>
2024-11-14 14:07:04 +0100

    feat: add firebase to project

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { FirebaseOptions } from "@angular/fire/app";
+
+export const firebaseConfig: FirebaseOptions = {
+  projectId: 'dance-blog',
+  appId: '1:458947448320:web:98b1c3f35739e6794f92e5',
+  storageBucket: 'dance-blog.firebasestorage.app',
+  apiKey: 'AIzaSyBjcUuemakp0wIbXD6U6X6Td4_JvM0fywU',
+  authDomain: 'dance-blog.firebaseapp.com',
+  messagingSenderId: '458947448320',
+  measurementId: 'G-J172NCQJ1W',
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,45 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { routes } from "./app.routes";
-import { CommonModule } from "@angular/common";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppComponent } from "./app.component";
-import { MainModule } from "./modules/main/main.module";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MainModule } from './modules/main/main.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { firebaseConfig } from './app.config';
 @NgModule({
-  providers: [provideAnimationsAsync()],
-  declarations: [
-    AppComponent,
-    NavbarComponent
+  providers: [
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
     CommonModule,
     MainModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
